e1a35342b25f2e5553b24dffbc303b5976e46a66
praesidi <edik.halilov1999@gmail.com>
2024-03-17 22:05:25 +0300

    import httpClient

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,11 +4,13 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
 	providers: [
 		provideAnimations(),
 		provideRouter(routes),
 		importProvidersFrom(TuiRootModule),
+		provideHttpClient(withFetch()),
 	],
 };
