b6183cea1b317edb291b8312b17bc4bd834043c0
praesidi <edik.halilov1999@gmail.com>
2024-03-17 18:36:35 +0300

    add routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,18 @@ import {
 } from '@taiga-ui/core';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
 	selector: 'app-root',
 	standalone: true,
-	imports: [RouterOutlet, TuiRootModule, TuiDialogModule, TuiAlertModule],
+	imports: [
+		RouterOutlet,
+		TuiRootModule,
+		TuiDialogModule,
+		TuiAlertModule,
+		HeaderComponent,
+	],
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.scss',
 	providers: [{ provide: TUI_SANITIZER, useClass: NgDompurifySanitizer }],
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { CitiesComponent } from './cities/cities.component';
+import { CountriesComponent } from './countries/countries.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: 'cities', component: CitiesComponent },
+	{ path: 'countries', component: CountriesComponent },
+	{ path: '**', redirectTo: 'countries', pathMatch: 'full' },
+];
+
+@NgModule({
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule],
+	declarations: [],
+})
+export class AppRoutingModule {}
